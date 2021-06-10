bd8a825716dd8240b2cbf49c02d1610ca955942a
Shreyashjiodndian2 <Shreyash.pandey.katni@gmail.com>
2021-06-10 11:13:13 +0530

    Updated MongoDB key

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGODB: 'mongodb+srv://Sachin:mernstack@cluster0.8n3ex.mongodb.net/merng?retryWrites=true&w=majority',
+    MONGODB: 'mongodb+srv://Sachin:aSMtbuFB7ikD1OCI@cluster0.8n3ex.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     SECRET_KEY: 'some very secret key'
 }
